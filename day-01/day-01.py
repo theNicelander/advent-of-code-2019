c45352158a3d324499c2b76e7c6e841e92bac45d
Petur Einarsson <petur.einarsson@bjss.com>
2019-12-02 14:52:25 +0000

    day 1 solutions

---
+++ day-01/day-01.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+df = pd.read_csv('input.txt', sep=' ', header=None)
+
+
+def get_fuel(mass: int) -> int:
+	fuel = int(mass // 3 - 2)
+	if fuel > 0:
+		return fuel
+	return 0
+
+
+df['fuel'] = df.apply(get_fuel, axis=1)
+
+# part 1 solution
+print(df['fuel'].sum())
+
+# part 2 requires recursion
+mass_list = df.loc[:, 0].values.tolist()
+
+total = 0
+for mass in mass_list:
+	fuel = get_fuel(mass)
+	while fuel > 0:
+		total += fuel
+		fuel = get_fuel(fuel)
+
+print(total)
